20771947a5a79375ec92362aacffb1c40782e70c
Reach420 <justreach555@gmail.com>
2021-03-07 16:56:58 +0700

    nav

---
+++ component/containers/header.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+export default function Header(){
+    return(
+        <Head>
+        <title>Title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+        </Head>
+    );
+}
---
+++ pages/boy.js
@@ -0,0 +1,7 @@
+export default function Boy(){
+    return(
+        <div>
+
+        </div>
+    )
+}
