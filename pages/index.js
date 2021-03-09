d6b84381b855916ea7d318fe18341917df1f4e1b
sokunthearoth <sokunthearoth.suon@gmail.com>
2021-03-09 20:08:56 +0700

    first

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,19 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p>hello</p>
+          <img src="image/logo.png" className={styles.logo} />
+          <form className={styles.form}>
+            <div className={styles.info}>
+              <input type="name" placeholder="First Name"/>
+              <input type="name" placeholder="Last Name"/>
+            </div>
+            <input type="email" placeholder="Email"/>
+            <input type="password" placeholder="Password"/>
+            <input type="password" placeholder="Confirm Password"/>
+            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+            <label for="vehicle1"> I accept the Term of Use & Privacy Policy .</label>
+            <button type="submit">Submit</button>
+          </form> 
     </div>
   )
 }
---
+++ pages/register.js
@@ -0,0 +1,9 @@
+import styles from './../styles/Register.module.css'
+
+export default function Register(){
+    return(
+        <div className={styles.Register}>
+
+        </div>
+    )
+}
