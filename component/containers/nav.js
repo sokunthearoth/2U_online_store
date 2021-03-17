39f4b971feb4d9928d35ba13873a2c7368fae1f4
Reach420 <justreach555@gmail.com>
2021-03-17 22:35:47 +0700

    home

--- component/containers/nav.js
+++ component/containers/nav.js
@@ -1,22 +1,21 @@
 import Link from '../presentations/link'
 import styles from './../../styles/Nav.module.css'
-export default function Nav(){
-    return(
+export default function Nav() {
+    return (
+        <div>
         <div className={styles.L}>
-            <div>
-        <Link href='/' name='Home'/>
-        <Link href='/contact' name='Contact'/>
-        <Link href='/about' name='About'/>
-        <Link href='/men' name='Men'/>
-        <Link href='/women' name='Women'/>
-        <Link href='/boy' name='Boy'/>
-        <Link href='/girl' name='Girl'/>
-        <Link href='/skincare' name='Skin Care'/>
-        <Link href='/signout' name='hi'><img src="sign.png"></img></Link>
-        <Link href='/cart' name='Cart'/>
-        <img src='NTU.png' className={styles.nav}></img>
+                <Link href='/home' className={styles.link}>Home</Link>
+                <Link href='/men' className={styles.link}>Men</Link>
+                <Link href='/women' className={styles.link}>Women</Link>
+                <Link href='/boy' className={styles.link}>Boy</Link>
+                <Link href='/girl' className={styles.link}>Girl</Link>
+                <Link href='/skincare' className={styles.link}>Skin Care</Link>
+                <Link href='/contact' className={styles.link}>Contacts</Link>
+                <Link href='/about' className={styles.link}>About</Link>
+                <Link href='/' ><img align="middle" className={styles.icon} src="sign.png"></img></Link>
+                <Link href='/cart'><img align="middle" className={styles.icon} src="cart.png"></img></Link>
+                <img src='NTU.png' className={styles.nav}></img>
         </div>
-        
         </div>
     )
 }
---
+++ pages/home.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/home.module.css'
+export default function Home() {
+    // const sliders = ["/cover_slider_1.jpg", "/cover_slider_4.jpg"];
+    // const index = 0;
+    // const slider_element = document.getElementById("slide");
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         slider_element.src = sliders[1];
+    //         index++;
+    //         if (index >= sliders.length) {
+    //             index = 0;
+    //         }
+    //     }, 1000);
+    //     return () => clearInterval(interval);
+    // }, []);
+    return (
+        <div>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="cover_slider_3.jpg" alt="First slide"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="cover_slider_3.jpg" alt="Second slide"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="cover_slider_3.jpg" alt="Third slide"></img>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+            <div className={styles.tb}>
+                <p className={styles.t1}>Welcome To 2U!</p>
+                <p className={styles.t2}>At the 2U online shop we offer a variety of clothing and skin care products that came from our partnered brands from all over the world.In our store we have up to 30+ items and more to come with the highest quality and shipping with our worldside global express. We value our customer very much so please enjoy your shopping trip at the 2U online store!</p>
+            </div>
+            <div className={styles.paralax}>
+            <p className={styles.p}>PRODUCT</p>
+            </div>
+        </div>
+    )
+}
