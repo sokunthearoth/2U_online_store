ba9b524f328d1026c13da540c97457c89b2ef8f6
sokunthearoth <sokunthearoth.suon@gmail.com>
2021-04-11 17:09:56 +0700

    up

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,4 @@
-import styles from './../styles/contact.module.css'
+import styles from './../styles/Contact.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faEnvelope, faMapMarkerAlt, faPhone} from "@fortawesome/free-solid-svg-icons"
 import ParallaxImage from '../component/presentations/parallax_image'
