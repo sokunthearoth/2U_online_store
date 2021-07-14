e61f10a13133eff7a40d61b000b39794c9f9f35d
Reach 420 <justreach555@gmail.com>
2021-07-14 13:09:54 +0700

    admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,64 +1,44 @@
 import { useState } from 'react';
-const Admin = () => {
-  // const user = await fetch('http://localhost:8000/api/users')
-  // const product = await fetch('http://localhost:8000/api/products')
-  // const userdata = await user.json()
-  // const productdata = await product.json()
-    const [name, setname] = useState('');
-    const [price, setprice] = useState('');
-    const [description, setdescription] = useState('');
-    const [category, setcategory] = useState('');
-    const [discount, setdiscount] = useState('');
-    const [instock, setinstock] = useState('');
-    const [img_url, setimg_url] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const product = { name,price,description,category,discount,instock,img_url };
-  
-      fetch('http://localhost:8000/api/products', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(product)
-      }).then((res) => {
-        console.log(res);
-      }).catch((err)=>{
-        console.log(err);
-      })
-    }
-  // const form = document.getElementById('form').value
-  // form.addEventListener('submit',(e) =>{
-  //   e.preventDefault()
-  //   const name = document.getElementById('name').value
-  //   const price = document.getElementById('price').value
-  //   const description = document.getElementById('description').value
-  //   const category = document.getElementById('category').value
-  //   const discount = document.getElementById('discount').value
-  //   const instock = document.getElementById('instock').value
-  //   const img_url = document.getElementById('img_url').value
-  //   fetch('http://localhost:8000/api/products'),{
-  //   method:'POST',
-  //   body:JSON.stringify({
-  //     name:name,
-  //     price:price,
-  //     description:description,
-  //     category:category,
-  //     discount:discount,
-  //     instock:instock,
-  //     img_url:img_url,
-  //   })
-  //   }
-    
-  // })
-  // return {
+export async function getStaticProps() {
+  const user = await fetch('http://localhost:8000/api/users')
+  const product = await fetch('http://localhost:8000/api/products')
+  const userdata = await user.json()
+  const productdata = await product.json()
+  return {
 
-  //   props: { userdata, productdata },
-  // }
+    props: { userdata, productdata },
+  }
+}
+const Admin = ({ userdata, productdata }) => {
+
+  const [name, setname] = useState('');
+  const [price, setprice] = useState('');
+  const [description, setdescription] = useState('');
+  const [category, setcategory] = useState('');
+  const [discount, setdiscount] = useState('');
+  const [instock, setinstock] = useState('');
+  const [img_url, setimg_url] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const product = { name, price, description, category, discount, instock, img_url };
+    fetch('http://localhost:8000/api/products', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(product)
+    }).then((res) => {
+      console.log(res);
+      console.log(product);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
 
 
   return (
     <div>
-      {/* <h1 style={{ textAlign: "center" }}>users</h1>
+      <h1 style={{ textAlign: "center" }}>users</h1>
       {
         userdata.map((user, index) => {
           return (
@@ -73,22 +53,22 @@ const Admin = () => {
         productdata.map((product, index) => {
           return (
             <div>
-              <h3 key={product._id} style={{ textAlign: "center" }}>{product.id} {product._id}</h3>
+              <h3 key={product._id} style={{ textAlign: "center" }}>{product.name} {product._id}</h3>
             </div>
           )
         })
-      } */}
+      }
       <form id='form' onSubmit={handleSubmit}>
-        <label>name:<input type="text" value={name} onChange={(e) => setname(e.target.value)} /></label><br></br>
-        <label>Price:<input type="text" value={price} onChange={(e) => setprice(e.target.value)} /></label><br></br>
-        <label>Category:<input type="text" value={category} onChange={(e) => setcategory(e.target.value)} /></label><br></br>
-        <label>Description:<input type="text" value={description} onChange={(e) => setdescription(e.target.value)} /></label><br></br>
-        <label>discount:<input type="text" value={discount} onChange={(e) => setdiscount(e.target.value)} /></label><br></br>
-        <label>Instock:<input type="text" value={instock} onChange={(e) => setinstock(e.target.value)} /></label><br></br>
-        <label>ImageURL:<input type="text" value={img_url} onChange={(e) => setimg_url(e.target.value)} /></label><br></br>
+        <label>name:<input type="text" value={name} name="name" onChange={(e) => setname(e.target.value)} /></label><br></br>
+        <label>Price:<input type="text" value={price} name="price" onChange={(e) => setprice(e.target.value)} /></label><br></br>
+        <label>Category:<input type="text" value={category} name="category" onChange={(e) => setcategory(e.target.value)} /></label><br></br>
+        <label>Description:<input type="text" value={description} name="description" onChange={(e) => setdescription(e.target.value)} /></label><br></br>
+        <label>discount:<input type="text" value={discount} name="discount" onChange={(e) => setdiscount(e.target.value)} /></label><br></br>
+        <label>Instock:<input type="text" value={instock} name="instock" onChange={(e) => setinstock(e.target.value)} /></label><br></br>
+        <label>ImageURL:<input type="text" value={img_url} name="img_url" onChange={(e) => setimg_url(e.target.value)} /></label><br></br>
         <input type="submit" value="submit"></input>
       </form>
     </div>
   );
-    };
+};
 export default Admin;
--- pages/home.js
+++ pages/home.js
@@ -6,7 +6,25 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Image from './../component/presentations/imageCart'
 import { ProductContext } from './../component/contexts/ProductContext'
-export default function Home() {
+export async function getStaticProps() {
+  const product = await fetch('http://localhost:8000/api/products')
+  const men_shirts = await fetch('http://localhost:8000/api/products/men_shirts')
+  const women = await fetch('http://localhost:8000/api/products?category=women')
+  const boy = await fetch('http://localhost:8000/api/products?category=boy')
+  const girl = await fetch('http://localhost:8000/api/products?category=girl')
+  const skincare = await fetch('http://localhost:8000/api/products/skincare')
+  const menshirtsdata = await men_shirts.json()
+  const womendata = await women.json()
+  const boydata = await boy.json()
+  const girldata = await girl.json()
+  const skincaredata = await skincare.json()
+  const productdata = await product.json()
+  return {
+    
+    props: {productdata, menshirtsdata, womendata, boydata, girldata, skincaredata},
+  }
+}
+export default function Home({productdata, menshirtsdata, womendata, boydata, girldata, skincaredata}) {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -26,218 +44,6 @@ export default function Home() {
       items: 1
     }
   };
-  const products = [
-    {
-      url: "hoddies.jpg",
-      name: "Plaid Shirt",
-      price: 10,
-      gender: "MEN",
-      type: "new-in",
-    },
-    {
-      url: "jecket.jpg",
-      name: "Coat",
-      price: 30,
-      gender: "MEN",
-      type: "new-in",
-    },
-    {
-      url: "product-image.jpg",
-      name: "Coat",
-      price: 25,
-      gender: "MEN",
-      type: "new-in",
-    },
-    {
-      url: "product_for_men_watch.jpg",
-      name: "Shirt",
-      price: 25,
-      gender: "MEN",
-      type: "new-in",
-    },
-    {
-      url: "trouser.jpg",
-      name: "Throuser",
-      price: 8,
-      gender: "MEN",
-      type: "new-in",
-    },
-    {
-      url: "product_for_men_T-shirt.jpg",
-      name: "Shirt",
-      price: 10,
-      gender: "MEN",
-      type: "new-in",
-    },
-    {
-      url: "short.jpg",
-      name: "Short",
-      price: 15,
-      gender: "MEN",
-      type: "new-in",
-    },
-    {
-      url: "set_clothe_women.jpg",
-      name: "Set_Shirt",
-      price: 15,
-      gender: "WOMEN",
-      type: "new-in"
-    },
-    {
-      url: "product_for_women_hoodie_pink.jpg",
-      name: "Shirt",
-      price: 10,
-      gender: "WOMEN",
-      type: "new-in"
-    },
-    {
-      url: "product_for_women_jean.jpg",
-      name: "Jean",
-      price: 18,
-      gender: "WOMEN",
-      type: "new-in"
-    },
-    {
-      url: "product_for_women_skirt.jpg",
-      name: "Skirt",
-      price: 15,
-      gender: "WOMEN",
-      type: "new-in"
-    },
-    {
-      url: "skirt.jpg",
-      name: "Skirt",
-      price: 15,
-      gender: "WOMEN",
-      type: "new-in"
-    },
-    {
-      url: "yellow_dress.jpg",
-      name: "Dress",
-      price: 10,
-      gender: "WOMEN",
-      type: "new-in"
-    },
-    {
-      url: "brown_dress.jpg",
-      name: "Dress",
-      price: 15,
-      gender: "WOMEN",
-      type: "new-in",
-    },
-    {
-      url: "blue_dress.jpg",
-      name: "Dress",
-      price: 20,
-      gender: "WOMEN",
-      type: "new-in",
-    },
-    {
-      url: "product_for_boy_set_T-shirt.jpg",
-      name: "T-shirt",
-      price: 10,
-      gender: "BOY",
-      type: "new-in",
-    },
-    {
-      url: "product_for_boy_sport_T-shirt.jpg",
-      name: "T-shirt",
-      price: 10,
-      gender: "BOY",
-      type: "new-in",
-    },
-    {
-      url: "product_for_boy_trousers.jpg",
-      name: "Trouser",
-      price: 10,
-      gender: "BOY",
-      type: "new-in",
-    },
-    {
-      url: "product_for_boy_shocks.jpg",
-      name: "Plaid Shirt",
-      price: 10,
-      gender: "BOY",
-      type: "new-in",
-    },
-    {
-      url: "product_for_boy_shoes.jpg",
-      name: "Trouser",
-      price: 10,
-      gender: "BOY",
-      type: "new-in",
-    },
-    {
-      url: "product_for_girl_dress.jpg",
-      name: "Dress",
-      price: 15,
-      gender: "GIRL",
-      type: "new-in",
-    },
-    {
-      url: "product_for_girl_trouser.jpg",
-      name: "Trouser",
-      price: 8,
-      gender: "GIRL",
-      type: "new-in",
-    },
-    {
-      url: "product_for_girl_Orange_clothes.jpg",
-      name: "Set-Dress Kid",
-      price: 15,
-      gender: "GIRL",
-      type: "new-in",
-    },
-    {
-      url: "set_clothe.jpg",
-      name: "Set-Shirt",
-      price: 15,
-      gender: "GIRL",
-      type: "new-in",
-    },
-    {
-      url: "hoddies_girl.jpg",
-      name: "Hoddie",
-      price: 15,
-      gender: "GIRL",
-      type: "new-in",
-    },
-    {
-      url: "pink_hoodie_girl.jpg",
-      name: "Hoddie",
-      price: 10,
-      gender: "GIRL",
-      type: "new-in",
-    },
-    {
-      url: "product_SkinCare.jpg",
-      name: "CLAM Skincare",
-      price: 15,
-      gender: "SKIN-CARE",
-      type: "new-in",
-    },
-    {
-      url: " skincare-set-dry.jpg",
-      name: "Bella Aura Skincare",
-      price: 20,
-      gender: "SKIN-CARE",
-      type: "new-in",
-    },
-    {
-      url: "Skin_care.jpg",
-      name: "Lightening Serum",
-      price: 25,
-      gender: "SKIN-CARE",
-      type: "new-in",
-    },
-    {
-      url: "Men_SkinCare.jpg",
-      name: "Best Face Mask",
-      price: 25,
-      gender: "SKIN-CARE",
-      type: "new-in",
-    },
-  ]
   const { cart, setcart } = useContext(ProductContext)
   useEffect(() => {
     document.title = `Home Page`;
@@ -294,9 +100,9 @@ export default function Home() {
           </div>
           <div className={styles.carousel_men}>
             <Carousel responsive={responsive}>
-              {products.filter(product => product.gender === "MEN").map((product, index) => {
+              {menshirtsdata.map((product, index) => {
                 return (
-                  <div><Image key={index} onclick={() => addcart(product)} src={product.url} title={product.name} price={product.price} /></div>
+                  <div><Image key={index} onclick={() => addcart(product)} src={product.img_url} title={product.name} price={product.price} /></div>
                 );
               }
               )}
@@ -314,9 +120,9 @@ export default function Home() {
           </div>
           <div className={styles.carousel_men}>
             <Carousel responsive={responsive}>
-              {products.filter(product => product.gender === "WOMEN").map((product, index) => {
+              {womendata.map((product, index) => {
                 return (
-                  <div><Image key={index} onclick={() => addcart(product)} src={product.url} title={product.name} price={product.price} /></div>
+                  <div><Image key={index} onclick={() => addcart(product)} src={product.img_url} title={product.name} price={product.price} /></div>
                 );
               }
               )}
@@ -334,9 +140,9 @@ export default function Home() {
           </div>
           <div className={styles.carousel_men}>
             <Carousel responsive={responsive}>
-              {products.filter(product => product.gender === "BOY").map((product, index) => {
+              {boydata.map((product, index) => {
                 return (
-                  <div><Image key={index} onclick={() => addcart(product)} src={product.url} title={product.name} price={product.price} /></div>
+                  <div><Image key={index} onclick={() => addcart(product)} src={product.img_url} title={product.name} price={product.price} /></div>
                 );
               }
               )}
@@ -354,9 +160,9 @@ export default function Home() {
           </div>
           <div className={styles.carousel_men}>
             <Carousel responsive={responsive}>
-              {products.filter(product => product.gender === "GIRL").map((product, index) => {
+              {girldata.map((product, index) => {
                 return (
-                  <div><Image key={index} onclick={() => addcart(product)} src={product.url} title={product.name} price={product.price} /></div>
+                  <div><Image key={index} onclick={() => addcart(product)} src={product.img_url} title={product.name} price={product.price} /></div>
                 );
               }
               )}
@@ -374,9 +180,9 @@ export default function Home() {
           </div>
           <div className={styles.carousel_men}>
             <Carousel responsive={responsive}>
-              {products.filter(product => product.gender === "SKIN-CARE").map((product, index) => {
+              {skincaredata.map((product, index) => {
                 return (
-                  <div><Image key={index} onclick={() => addcart(product)} src={product.url} title={product.name} price={product.price} /></div>
+                  <div><Image key={index} onclick={() => addcart(product)} src={product.img_url} title={product.name} price={product.price} /></div>
                 );
               }
               )}
